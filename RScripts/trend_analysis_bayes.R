130ee44f489f75b6fe226511cb4891c52332021d
bridgetmarie24 <bridgetbittmann@u.boisestate.edu>
2023-05-03 15:35:23 -0600

    create trend plots

--- RScripts/trend_analysis_bayes.R
+++ RScripts/trend_analysis_bayes.R
@@ -17,6 +17,7 @@ library(brms) # to run generalized linear models in bayesian
 library(tidybayes) # taking posterior draws from the model
 library(ggplot2) # plotting 
 library(modelr) # for data grid when drawing expected predictions
+library(ggpubr) # multiplot figure
 
 # ---------------- #
 # Set Directory ####
@@ -140,8 +141,58 @@ names_neg <- unique(subset(year_effect, Estimate < 0 & sig == 'yes'))
 pos_names <- unique(names_pos$Name)
 neg_names <- unique(names_neg$Name)
 
+# Positive predictions and data
 pos_preds <- list()
 for (i in pos_names){
-  
+  data <- all_mods[[i]]
+  preds <- data$edraws
+  preds$Name <- i
+  pos_preds[[i]] <- preds
+}
+pos_preds <- bind_rows(pos_preds) # All predictions in one dataframe
+pos_median <- pos_preds %>%
+  group_by(Name, Year) %>%
+  summarise(med.pred = median(.epred)) # get median prediction to plot, don't want CI
+pos_div <- subset(div_data, Name %in% pos_names) # Raw points for trend plot
+
+neg_preds <- list()
+for (i in neg_names){
+  data <- all_mods[[i]]
+  preds <- data$edraws
+  preds$Name <- i
+  neg_preds[[i]] <- preds
+} # pull negative trend predictions from all the models
+neg_preds <- bind_rows(neg_preds) # all predictions in one dataframe
+neg_median <- neg_preds %>%
+  group_by(Name, Year) %>%
+  summarise(med.pred = median(.epred)) # get median prediction to plot, don't want CI
+neg_div <- subset(div_data, Name %in% neg_names) # get point data for plots
+
+# ---------------------------------------------------- #
+# Create trend plots for large and small diversions ####
+# ---------------------------------------------------- # 
+
+trend_plot <- function(df.pred, df.data, ylimit){
+  plt <- ggplot(data = df.pred, aes(x = Year, y = med.pred, group = Name)) +
+    geom_point(data = df.data, aes(x = Year, y = Acre_feet), shape = 1) +
+    geom_line(color = 'black') +
+    theme_bw() +
+    ylab('Discharge (Acre-feet)') +
+    coord_cartesian(ylim = ylimit) +
+    theme(text = element_text(size = 13)) +
+    scale_y_continuous(labels = scales::comma)
+  return(plt)
 }
 
+small.pos <- trend_plot(pos_median, pos_div, c(0,22000)) # small diversions, positive trends
+small.neg <- trend_plot(neg_median, neg_div, c(0,22000)) # small diversions, negative trends
+big.pos <- trend_plot(pos_median, pos_div, c(30000, 110000)) + ggtitle('Increasing Trends') # big diversions, positive trends
+big.neg <- trend_plot(neg_median, neg_div, c(30000, 110000)) + ggtitle('Decreasing Trends') # big diversions, negateive trends
+
+all_trend <- ggarrange(plotlist = list(big.pos, big.neg, small.pos, small.neg), ncol = 2, nrow = 2, 
+          labels = c('A', 'B', 'C', 'D'))
+ggsave(paste(cd, '/ManuscriptFigures/trend_bayes.svg', sep = ''), 
+       plot = all_trend,
+       width = 10,
+       height = 10,
+       units = 'in')
