c047f9fd216d56e65ed7e83ced0fd031784b65b0
bridgetmarie24 <bridgetbittmann@u.boisestate.edu>
2025-09-01 17:13:10 -0500

    determined how to test for stationarity in resdiuals

--- RScripts/check_residuals.R
+++ RScripts/check_residuals.R
@@ -1,11 +1,22 @@
 library(brms)
 library(plm)
 
-cd_models <- '~/Desktop/diversion_models/final_models/'
-mod.arma <- readRDS(paste(cd_models, 'mod-arma-stud-041123.RDS', sep = ''))
+working_dir <- 'Desktop/temp-dir/pod_pou_lulcc/'
 
-res <- residuals(mod.arma)
-meanres <- apply(res, 2, mean)
+# Read in the model
+mod.arma <- readRDS(paste(working_dir, 'models/mod-arma-stud-041123.RDS', sep = ''))
+
+# Grab the residuals
+res <- residuals(mod.arma, summary = FALSE)
+meanresids <- apply(res, 2, mean)
+
+# Read in the original data to get group and year data
+data <- read.csv(paste(working_dir, 'data/arma_input_041123.csv', sep = ''))
+data$res <- meanresids
+
+resid_df <- data[, c('Name', 'Year', 'res')]
+resid_pframe <- pdata.frame(resid_df, index = c('Name', 'Year'))
 
 # test for stationarity
-purtest(res, test = 'levinlin')
+purtest(resid_pframe$res, data = resid_pframe, lags = 'AIC', test = 'levinlin') 
+
